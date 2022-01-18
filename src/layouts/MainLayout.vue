b212c0d6fd9f3b6cff6fcfa22eaa94fd5ef7ef7b
emerson fausto lisboa - 200007 <emerson.lisboa@fxnbr.com>
2022-01-18 15:20:51 -0500

    Ajusted main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,9 +11,7 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-         Equipamentor
-        </q-toolbar-title>
+        <q-toolbar-title> Equipamentor </q-toolbar-title>
 
         <div>Version v{{ $q.version }}</div>
       </q-toolbar>
@@ -23,20 +21,22 @@
       v-model="leftDrawerOpen"
       show-if-above
       bordered
+      :mini="miniState"
+
+      @mouseover="miniState = false"
+      @mouseout="miniState = true"
     >
-      <q-list>
-        <q-item-label
-          header
-        >
+  
+        <q-list>
+          <q-item-label header> </q-item-label>
 
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+          <EssentialLink
+            v-for="link in essentialLinks"
+            :key="link.title"
+            v-bind="link"
+          />
+        </q-list>
+    
     </q-drawer>
 
     <q-page-container>
@@ -46,90 +46,107 @@
 </template>
 
 <script lang="ts">
-import EssentialLink from 'components/EssentialLink.vue'
-
+import EssentialLink from "components/EssentialLink.vue";
 
 const linksList = [
-
   {
-    title: 'Dashboard',
-    icon: 'dashboard',
-    to: '/'
-  },
-
-   {
-    title: 'Ordem de Trabalho',
-    caption: 'WorkOrders',
-    icon: 'assignment',
-    to: '/workOrder'
+    title: "Dashboard",
+    icon: "dashboard",
+    to: "/",
   },
 
   {
-    title: 'Solicitações',
-     caption: 'Requests',
-    icon: 'add',
-    to: '/workOrder'
-  },
-
-   {
-    title: 'Locais',
-    caption: 'Locations',
-    icon: 'place',
-    to: '/workOrder'
-  },
-    {
-    title: 'Equipamentos',
-    caption: 'Assets',
-    icon: 'settings',
-    to: '/list-equipment'
-  },
-    {
-    title: 'Manutenção Preventiva (PM)',
-    caption: 'Preventive Maintenance',
-    icon: 'build',
-    to: '/listLocal'
-  },    {
-    title: 'Departmento',
-    caption: 'Department',
-    icon: 'account_balance',
-    to: '/addDepartment'
+    title: "Ordem de Trabalho",
+    caption: "WorkOrders",
+    icon: "assignment",
+    to: "/workOrder",
   },
 
   {
-    title: 'Funcionário',
-    caption: 'Funcionário',
-    icon: 'people',
-    to: '/Department'
+    title: "Solicitações",
+    caption: "Requests",
+    icon: "add",
+    to: "/workOrder",
   },
 
   {
-    title: 'Configurações',
-    caption: 'Settings',
-    icon: 'settings',
-    to: '/Department1'
+    title: "Locais",
+    caption: "Locations",
+    icon: "place",
+    to: "/workOrder",
+  },
+  {
+    title: "Equipamentos",
+    caption: "Assets",
+    icon: "settings",
+    to: "/list-equipment",
+  },
+  {
+    title: "Manutenção Preventiva (PM)",
+    caption: "Preventive Maintenance",
+    icon: "build",
+    to: "/listLocal",
+  },
+  {
+    title: "Departmento",
+    caption: "Department",
+    icon: "account_balance",
+    to: "/addDepartment",
+  },
+
+  {
+    title: "Funcionário",
+    caption: "Funcionário",
+    icon: "people",
+    to: "/Department",
+  },
+
+  {
+    title: "Configurações",
+    caption: "Settings",
+    icon: "settings",
+    to: "/Department1",
   },
 ];
 
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
     EssentialLink,
-
   },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
 
     return {
+      drawer: ref(false),
+      miniState: ref(true),
+      contentStyle: {
+        backgroundColor: "rgba(0,0,0,0.02)",
+        color: "#555",
+      },
+
+      contentActiveStyle: {
+        color: "black",
+      },
+
+      thumbStyle: {
+        right: "2px",
+        borderRadius: "5px",
+        backgroundColor: "#027be3",
+        width: "5px",
+        opacity: 0.75,
+      },
+
       essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
 </script>
